b3f1d4047d09ac841bd149198849ca3768163134
reon <reonsparow@gmail.com>
2020-06-06 19:21:00 +0600

    Node API setup

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// bring routes
+const blogRoutes = require('./routes/blog');
+
+// app
+const app = express();
+mongoose
+  .connect(process.env.DATABASE_CLOUD, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log(`DB connected on CLOUD`));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// cors
+if (process.env.NODE_ENV == 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+
+//middle routes
+app.use('/api', blogRoutes);
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
